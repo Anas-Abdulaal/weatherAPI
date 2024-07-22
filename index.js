9febd895439dcd105c6780a7c113f3292fe52116
Anas-Abdulaal <abdulaalanas@gmail.com>
2024-07-22 15:10:17 +0200

    port from environment variable

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express();
 const Datastore = require("nedb");
 const database = new Datastore("database.db");
 database.loadDatabase();
-app.listen(3000, () =>{ console.log("listening at 3000") });
+const port = process.env.PORT || 3000;
+app.listen(port, () =>{ console.log(`listening at ${port}`) });
 app.use(express.static("public"));
 app.use(express.json({limit: "1mb"}));
 
